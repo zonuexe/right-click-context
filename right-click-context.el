160716a244f8e4722810ef8399e2035d6d46e8b3
USAMI Kenta <tadsan@zonu.me>
2018-12-11 12:45:24 +0900

    Modify commentary

--- right-click-context.el
+++ right-click-context.el
@@ -27,7 +27,20 @@
 
 ;;; Commentary:
 
-;;;     (right-click-context-mode 1)
+;; This mode focuses on providing operations similar to GUI context menus.
+;; It not only activates commands, it also supports operations on Region.
+;;
+;; Put the following into your .emacs file (~/.emacs.d/init.el) to enable context menu.
+;;
+;;     (right-click-context-mode 1)
+;;
+;; This function does not depend on GUI, it is fully available on terminal.
+;; The menu is launched by "right click" (<mouse-3>) by default, but you can assign any key.
+;;
+;;     (define-key right-click-context-mode-map (kbd "C-c :") 'right-click-context-menu)
+;;
+;; This menu can be constructed with a simple DSL based on S-expression.
+;; Additional information can be found in README and implementation code
 
 ;;; Code:
 (require 'cl-lib)
