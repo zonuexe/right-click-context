ee90781b2b2ad27aa4140c307ac59bbd79fb8a59
USAMI Kenta <tadsan@zonu.me>
2021-05-20 02:06:13 +0900

    Remove (cl-lib "0.5") from Package-Requires

--- right-click-context.el
+++ right-click-context.el
@@ -5,7 +5,7 @@
 ;; Author: USAMI Kenta <tadsan@zonu.me>
 ;; Created: 8 May 2016
 ;; Version: 0.3.0
-;; Package-Requires: ((emacs "24.3") (cl-lib "0.5") (popup "0.5") (ordinal "0.0.1"))
+;; Package-Requires: ((emacs "24.3") (popup "0.5") (ordinal "0.0.1"))
 ;; Keywords: mouse menu rightclick
 ;; Homepage: https://github.com/zonuexe/right-click-context
 ;; License: GPL-3.0-or-later
